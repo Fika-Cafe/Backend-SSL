049d9b5ac4595e55fba0e46a8d8af71a3b351ee7
Eduardo Arroyo <aeduardoiran@gmail.com>
2025-07-23 21:54:31 -0700

    feat: Se configuro para aceptar json y urlencoded

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
